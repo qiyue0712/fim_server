afe9c152aebcaa882f2d6ef2b805e60d7056c93a
qiyue0712 <13641913653@163.com>
2025-12-13 17:59:14 +0800

    群验证表

--- fim_group/models/group_verify_model.go
+++ fim_group/models/group_verify_model.go
@@ -5,12 +5,13 @@ import (
 	"fim_server/common/models/ctype"
 )
 
-// FriendVerifyModel 好友验证表
-type FriendVerifyModel struct {
+// GroupVerifyModel 群验证消息
+type GroupVerifyModel struct {
 	models.Model
-	SendUserID           uint                        `json:"sendUserID"`
-	RevUserID            uint                        `json:"revUserID"`
+	GroupID              uint                        `json:"groupID"`              // 群id
+	UserID               uint                        `json:"userID"`               // 需要加群或者是退群的用户id
 	Status               int8                        `json:"status"`               // 状态 0 未操作 1 同意 2 拒绝 3 忽略
 	AdditionalMessages   string                      `json:"additionalMessages"`   // 附加消息
 	VerificationQuestion *ctype.VerificationQuestion `json:"verificationQuestion"` // 验证问题 3和4的时候需要
+	Type                 int8                        `json:"type"`                 // 类型 1 加群 2 退群
 }

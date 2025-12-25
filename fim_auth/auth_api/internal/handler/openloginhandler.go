1cece8807bd4c911e4b739b34fc3c496abcc0bff
qiyue0712 <13641913653@163.com>
2025-12-25 09:41:14 +0800

    auth_api.api

---
+++ fim_auth/auth_api/internal/handler/openloginhandler.go
@@ -0,0 +1,24 @@
+// Code scaffolded by goctl. Safe to edit.
+// goctl 1.9.2
+
+package handler
+
+import (
+	"net/http"
+
+	"fim_server/fim_auth/auth_api/internal/logic"
+	"fim_server/fim_auth/auth_api/internal/svc"
+	"github.com/zeromicro/go-zero/rest/httpx"
+)
+
+func open_loginHandler(svcCtx *svc.ServiceContext) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		l := logic.NewOpen_loginLogic(r.Context(), svcCtx)
+		resp, err := l.Open_login()
+		if err != nil {
+			httpx.ErrorCtx(r.Context(), w, err)
+		} else {
+			httpx.OkJsonCtx(r.Context(), w, resp)
+		}
+	}
+}
